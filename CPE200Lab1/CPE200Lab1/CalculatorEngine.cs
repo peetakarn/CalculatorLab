f1116d1e59c26821641385ecf24dc4a6b04d15a9
peetakarn <peetakarn.peach@me.com>
2017-09-08 13:16:11 +0700

    Approved.

--- CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -47,6 +48,29 @@ namespace CPE200Lab1
                 return "Error";
             }
             return returnTo;
+        }
+        public string Process(string str)
+        {
+            string result = "Zero.";
+            Stack NumList = new Stack();
+            string[] parts = str.Split(' ');
+            if (parts[(parts.Length) - 1] == "") return "E";
+            for (int i = parts.Length - 1; i >= 0; i--)
+            {
+                if (isNumber(parts[i]))
+                {
+                    NumList.Push(parts[i]);
+                }
+            }
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (isOperator(parts[i]))
+                {
+                    result = calculate(parts[i], NumList.Pop().ToString(), NumList.Pop().ToString());
+                    NumList.Push(result);
+                }
+            }
+            return NumList.Pop().ToString();
         }
         public string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
         {
