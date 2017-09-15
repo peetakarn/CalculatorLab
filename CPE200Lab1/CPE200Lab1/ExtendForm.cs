9d37659a47f16226b43e37b5359616e7ce47ce95
peetakarn <peetakarn.peach@me.com>
2017-09-15 15:05:55 +0700

    ok

--- CPE200Lab1/CPE200Lab1/ExtendForm.cs
+++ CPE200Lab1/CPE200Lab1/ExtendForm.cs
@@ -15,12 +15,12 @@ namespace CPE200Lab1
         private bool isNumberPart = false;
         private bool isContainDot = false;
         private bool isSpaceAllowed = false;
-        private CalculatorEngine engine;
+        private RPNCalculatorEngines engine;
 
         public ExtendForm()
         {
             InitializeComponent();
-            engine = new CalculatorEngine();
+            engine = new RPNCalculatorEngines();
         }
 
         private bool isOperator(char ch)
@@ -65,8 +65,12 @@ namespace CPE200Lab1
             string current = lblDisplay.Text;
             if (current[current.Length - 1] != ' ')
             {
-                lblDisplay.Text += " " + ((Button)sender).Text + " ";
+                lblDisplay.Text += " "+((Button)sender).Text+ " ";
                 isSpaceAllowed = false;
+            }
+            else
+            {
+                lblDisplay.Text += ((Button)sender).Text;
             }
         }
 
@@ -150,6 +154,11 @@ namespace CPE200Lab1
                 isContainDot = true;
                 lblDisplay.Text += ".";
                 isSpaceAllowed = false;
+            }
+            else
+            {
+                isContainDot = true;
+                lblDisplay.Text += ".";
             }
         }
 
